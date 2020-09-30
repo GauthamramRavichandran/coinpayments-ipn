bf2f5560b769027a276897c87c95c16aaa070dd1
GauthamramRavichandran <30320759+GauthamramRavichandran@users.noreply.github.com>
2020-09-30 08:34:34 +0530

    Create ipn_handler_server.py
    simple flask app

---
+++ ipn_handler_server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import hmac
+import hashlib
+
+app = Flask(__name__)
+
+SECRET = 'itsnotmine'
+@app.route("/ipn/", methods = ["POST"])
+def receive_ipn():
+	received_hmac = request.headers['Hmac'])
+	generated_hmac = hmac.new(key = bytearray(SECRET, 'utf-8'), 
+                              msg = request.get_data(), # raw POST data will be passed here
+                              digestmod = hashlib.sha512).hexdigest()
+	if received_hmac == generated_hmac:
+    pass  # do your thing here, notify user, save txn in DB ...
+  else:
+    pass # ignore, since it's not the right one
+
+
+if __name__ == "__main__":
+	app.run()
