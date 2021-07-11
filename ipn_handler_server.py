a53f2dffdf7202e86d2751f1143382068b5fdd8e
Javad <mail@heidarpoor.ir>
2021-07-11 10:08:21 +0430

    empty request data changed to request query to create sha512 of request

--- ipn_handler_server.py
+++ ipn_handler_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import hmac
 import hashlib
+from urllib.parse import urlencode
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ SECRET = 'itsnotmine'
 def receive_ipn():
 	received_hmac = request.headers['Hmac'])
 	generated_hmac = hmac.new(key = bytearray(SECRET, 'utf-8'), 
-                              msg = request.get_data(), # raw POST data will be passed here
+                              msg = urlencode(request.form.to_dict()).encode('ascii'), # raw POST data will be passed here
                               digestmod = hashlib.sha512).hexdigest()
 	if received_hmac == generated_hmac:
    	  pass  # do your thing here, notify user, save txn in DB ...
